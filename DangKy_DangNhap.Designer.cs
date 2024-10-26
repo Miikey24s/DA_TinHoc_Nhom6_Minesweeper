e2d7282d9ab8a75e4e5978336cfa904c298fdcd0
Nam Nguyễn <annamnguyen04@gmail.com>
2024-10-26 20:33:01 +0700

    thêm button khách

--- DangKy_DangNhap.Designer.cs
+++ DangKy_DangNhap.Designer.cs
@@ -34,70 +34,88 @@
             this.txtMatKhau = new System.Windows.Forms.TextBox();
             this.btnDangKy = new System.Windows.Forms.Button();
             this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnKhách = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTaiKhoan
             // 
             this.lblTaiKhoan.AutoSize = true;
-            this.lblTaiKhoan.Location = new System.Drawing.Point(137, 94);
+            this.lblTaiKhoan.Location = new System.Drawing.Point(68, 49);
+            this.lblTaiKhoan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTaiKhoan.Name = "lblTaiKhoan";
-            this.lblTaiKhoan.Size = new System.Drawing.Size(107, 25);
+            this.lblTaiKhoan.Size = new System.Drawing.Size(55, 13);
             this.lblTaiKhoan.TabIndex = 0;
             this.lblTaiKhoan.Text = "Tài khoản";
             // 
             // lblMatKhau
             // 
             this.lblMatKhau.AutoSize = true;
-            this.lblMatKhau.Location = new System.Drawing.Point(137, 156);
+            this.lblMatKhau.Location = new System.Drawing.Point(68, 81);
+            this.lblMatKhau.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMatKhau.Name = "lblMatKhau";
-            this.lblMatKhau.Size = new System.Drawing.Size(101, 25);
+            this.lblMatKhau.Size = new System.Drawing.Size(52, 13);
             this.lblMatKhau.TabIndex = 2;
             this.lblMatKhau.Text = "Mật khẩu";
             this.lblMatKhau.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtTaiKhoan
             // 
-            this.txtTaiKhoan.Location = new System.Drawing.Point(287, 88);
+            this.txtTaiKhoan.Location = new System.Drawing.Point(144, 46);
+            this.txtTaiKhoan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTaiKhoan.Name = "txtTaiKhoan";
-            this.txtTaiKhoan.Size = new System.Drawing.Size(382, 31);
+            this.txtTaiKhoan.Size = new System.Drawing.Size(193, 20);
             this.txtTaiKhoan.TabIndex = 3;
             // 
             // txtMatKhau
             // 
-            this.txtMatKhau.Location = new System.Drawing.Point(287, 153);
+            this.txtMatKhau.Location = new System.Drawing.Point(144, 80);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMatKhau.Name = "txtMatKhau";
-            this.txtMatKhau.Size = new System.Drawing.Size(382, 31);
+            this.txtMatKhau.Size = new System.Drawing.Size(193, 20);
             this.txtMatKhau.TabIndex = 4;
             // 
             // btnDangKy
             // 
-            this.btnDangKy.Location = new System.Drawing.Point(207, 388);
+            this.btnDangKy.Location = new System.Drawing.Point(104, 202);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(241, 127);
+            this.btnDangKy.Size = new System.Drawing.Size(120, 66);
             this.btnDangKy.TabIndex = 5;
             this.btnDangKy.Text = "Đăng ký";
             this.btnDangKy.UseVisualStyleBackColor = true;
             // 
             // btnDangNhap
             // 
-            this.btnDangNhap.Location = new System.Drawing.Point(649, 388);
+            this.btnDangNhap.Location = new System.Drawing.Point(228, 202);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(241, 127);
+            this.btnDangNhap.Size = new System.Drawing.Size(120, 66);
             this.btnDangNhap.TabIndex = 6;
             this.btnDangNhap.Text = "Đăng nhập";
             this.btnDangNhap.UseVisualStyleBackColor = true;
             // 
+            // btnKhách
+            // 
+            this.btnKhách.Location = new System.Drawing.Point(353, 202);
+            this.btnKhách.Name = "btnKhách";
+            this.btnKhách.Size = new System.Drawing.Size(120, 66);
+            this.btnKhách.TabIndex = 7;
+            this.btnKhách.Text = "Khách";
+            this.btnKhách.UseVisualStyleBackColor = true;
+            // 
             // DangKy_DangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 702);
+            this.ClientSize = new System.Drawing.Size(589, 365);
+            this.Controls.Add(this.btnKhách);
             this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.btnDangKy);
             this.Controls.Add(this.txtMatKhau);
             this.Controls.Add(this.txtTaiKhoan);
             this.Controls.Add(this.lblMatKhau);
             this.Controls.Add(this.lblTaiKhoan);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DangKy_DangNhap";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -112,5 +130,6 @@
         private System.Windows.Forms.TextBox txtMatKhau;
         private System.Windows.Forms.Button btnDangKy;
         private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnKhách;
     }
 }
