955eefd83b3d818cb18e6daead0a23df274198e8
1uan-3 <luanbdl8@gmail.com>
2024-10-26 22:51:08 +0700

    TrangChu

--- TrangChu.Designer.cs
+++ TrangChu.Designer.cs
@@ -41,20 +41,22 @@
             this.lblTuaDe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblTuaDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.lblTuaDe.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.lblTuaDe.Location = new System.Drawing.Point(186, 66);
+            this.lblTuaDe.Location = new System.Drawing.Point(239, 81);
+            this.lblTuaDe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTuaDe.Name = "lblTuaDe";
-            this.lblTuaDe.Size = new System.Drawing.Size(400, 50);
+            this.lblTuaDe.Size = new System.Drawing.Size(533, 62);
             this.lblTuaDe.TabIndex = 0;
-            this.lblTuaDe.Text = "DÒ MINE ĐÊ!!!";
+            this.lblTuaDe.Text = "DÒ MÌN ĐÊ!!!";
             this.lblTuaDe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnBatDau
             // 
             this.btnBatDau.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnBatDau.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnBatDau.Location = new System.Drawing.Point(301, 203);
+            this.btnBatDau.Location = new System.Drawing.Point(401, 250);
+            this.btnBatDau.Margin = new System.Windows.Forms.Padding(4);
             this.btnBatDau.Name = "btnBatDau";
-            this.btnBatDau.Size = new System.Drawing.Size(144, 51);
+            this.btnBatDau.Size = new System.Drawing.Size(192, 63);
             this.btnBatDau.TabIndex = 1;
             this.btnBatDau.Text = "BẮT ĐẦU";
             this.btnBatDau.UseVisualStyleBackColor = true;
@@ -63,21 +65,24 @@
             // btnHuongDan
             // 
             this.btnHuongDan.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnHuongDan.Location = new System.Drawing.Point(269, 294);
+            this.btnHuongDan.Location = new System.Drawing.Point(359, 362);
+            this.btnHuongDan.Margin = new System.Windows.Forms.Padding(4);
             this.btnHuongDan.Name = "btnHuongDan";
-            this.btnHuongDan.Size = new System.Drawing.Size(208, 52);
+            this.btnHuongDan.Size = new System.Drawing.Size(277, 64);
             this.btnHuongDan.TabIndex = 2;
             this.btnHuongDan.Text = "HƯỚNG DẪN";
             this.btnHuongDan.UseVisualStyleBackColor = true;
+            this.btnHuongDan.Click += new System.EventHandler(this.btnHuongDan_Click);
             // 
             // TrangChu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 450);
+            this.ClientSize = new System.Drawing.Size(1031, 554);
             this.Controls.Add(this.btnHuongDan);
             this.Controls.Add(this.btnBatDau);
             this.Controls.Add(this.lblTuaDe);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TrangChu";
             this.Text = "Form1";
             this.ResumeLayout(false);
